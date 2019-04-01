ca23a025dc93409e09d15b87bba229153e76d851
Ngô Minh Sao <ngominhsaoS2@gmail.com>
2019-04-02 06:32:40 +0700

    End of section 15

--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -10,6 +10,7 @@ import { MemberListResolver } from './_resolvers/member-list.resolver';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { MemberEditResolver } from './_resolvers/member-edit.resolver';
 import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
+import { ListsResolver } from './_resolvers/lists.resolver';
 
 export const appRoutes: Routes = [
   { path: '', component: HomeComponent },
@@ -19,20 +20,30 @@ export const appRoutes: Routes = [
     canActivate: [AuthGuard],
     children: [
       {
-        path: 'members', component: MemberListComponent,
+        path: 'members',
+        component: MemberListComponent,
         resolve: { users: MemberListResolver }
       },
       {
-        path: 'members/:id', component: MemberDetailComponent,
+        path: 'members/:id',
+        component: MemberDetailComponent,
         resolve: { user: MemberDetailResolver }
       },
       {
-        path: 'member/edit', component: MemberEditComponent,
+        path: 'member/edit',
+        component: MemberEditComponent,
         resolve: { user: MemberEditResolver },
         canDeactivate: [PreventUnsavedChanges]
       },
-      { path: 'messages', component: MessagesComponent },
-      { path: 'lists', component: ListsComponent },
+      {
+        path: 'messages',
+        component: MessagesComponent
+      },
+      {
+        path: 'lists',
+        component: ListsComponent,
+        resolve: { users: ListsResolver }
+      },
     ]
   },
   { path: '**', redirectTo: '', pathMatch: 'full' }
